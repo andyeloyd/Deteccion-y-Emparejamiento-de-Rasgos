7bac3314bc0986f98c6f17749d4b15ad13e2e7ac
andyeloyd <54008991+andyeloyd@users.noreply.github.com>
2020-04-04 13:25:52 -0700

    Update emparejamiento.py

--- emparejamiento.py
+++ emparejamiento.py
@@ -1,6 +1,3 @@
-# Falta corroborar el correcto funcionamiento de los metodos
-# Tambien falta implementar LSH
-
 import numpy as np
 import cv2 as cv
 import os.path
@@ -17,7 +14,10 @@ def matching(t):
         detector = cv.KAZE_create()
     # BRIEF
     elif detectMethod == '4':
-        detector = cv.xfeatures2d.StarDetector_create()
+        # Se crea un detector "star" para keypoints
+        # y un detector "brief" para descriptores
+        star = cv.xfeatures2d.StarDetector_create()
+        detector = cv.xfeatures2d.BriefDescriptorExtractor_create()
     # BRISK
     elif detectMethod == '5':
         detector = cv.BRISK_create()
@@ -29,27 +29,47 @@ def matching(t):
         detector = cv.AKAZE_create()
     else:
         return
-    kp1, desc1 = detector.detectAndCompute(img1, None)
-    kp2, desc2 = detector.detectAndCompute(img2, None)
+
+    # Si se emplea BRIEF se realizan las operaciones de deteccion
+    # y computacion por separado, pues BRIEF no posee metodo .detectAndCompute
+
+    if detectMethod != '4':
+        # Si no se usa BRIEF
+        kp1, desc1 = detector.detectAndCompute(img1, None)
+        kp2, desc2 = detector.detectAndCompute(img2, None)
+    else:
+        # Si se usa BRIEF:
+        # Se detectan los rasgos con star
+        kp1 = star.detect(img1, None)
+        kp2 = star.detect(img1, None)
+        #Se obtienen los descriptores con brief
+        kp1, desc1 = detector.compute(img1, kp1)
+        kp2, desc2 = detector.compute(img2, kp2)
 
     if matchMethod == '1':
         if int(detectMethod) <= 3:
-            #matcher = cv.DescriptorMatcher_BRUTEFORCE_L1
             matcher = cv.BFMatcher(cv.NORM_L2, crossCheck=False)
         else:
-            #matcher = cv.DescriptorMatcher_BRUTEFORCE_HAMMING
             matcher = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=False)
+
     elif matchMethod == '2':
         if int(detectMethod) <= 3:
-            matcher = cv.DescriptorMatcher_FLANNBASED
+            FLANN_INDEX_KDTREE = 0
+            index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+            search_params = dict(checks=50)  # or pass empty dictionary
+            matcher = cv.FlannBasedMatcher(index_params, search_params)
+
         else:
-            # TO DO: FLANNBASED LSH
-            matcher = cv.DescriptorMatcher_FLANNBASED
+            FLANN_INDEX_LSH = 6
+            index_params = dict(algorithm=FLANN_INDEX_LSH, table_number=6,
+                                key_size=12, multi_probe_level=1)
+            search_params = dict(checks=50)  # or pass empty dictionary
+            matcher = cv.FlannBasedMatcher(index_params, search_params)
     else:
         return
     matches = matcher.knnMatch(desc1, desc2, k=2, mask=None)
 
-    # Filtering out matches
+    # Filtrado de emparejamientos
     good_matches = []
     for m, n in matches:
         if m.distance < t * n.distance:
@@ -63,11 +83,7 @@ def matching(t):
     cv.drawMatches(img1, kp1, img2, kp2, good_matches, img_matches,
                    flags = cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
 
-
-    #img3 = cv.drawMatches(img1, kp1, img2, kp2, matches[:10], None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    # Show image with detected matches
-    #plt.imshow(img3), plt.show()
-    cv.imshow('Emparejamientos de Rasgos', img_matches)
+    cv.imshow('Emparejamiento de Rasgos', img_matches)
 
 # Entrada de imagen de referencia
 archivo = input('Nombre de la imagen de referencia: \n')
@@ -86,7 +102,7 @@ img2 = cv.imread(archivo, 0)
 # Seleccion del metodo de deteccion de rasgos
 print('\nMetodos de Deteccion de Rasgos\nDescriptores de punto flotante:\n'
       '(1)SIFT\n(2)SURF\n(3)KAZE\n'
-      'Descriptores binarios:\n (4)BRIEF\n(5)BRISK\n(6)ORB\n(7)AKAZE\n')
+      'Descriptores binarios:\n(4)BRIEF\n(5)BRISK\n(6)ORB\n(7)AKAZE\n')
 detectMethod = input('Seleccione el metodo: ')
 
 # Selecciion del metodo de emparejamiento
@@ -94,7 +110,7 @@ print('Metodos de emparejamiento:\n (1) Fuerza Bruta\n (2) FLANN\n')
 matchMethod = input('Seleccione el metodo: ')
 
 # Crea una ventana y muetra la imagen
-cv.namedWindow('Emparejamiento de Rasgos', cv.WINDOW_NORMAL)
+#cv.namedWindow('Emparejamiento de Rasgos', cv.WINDOW_NORMAL)
 
 matching(0.7)
 
